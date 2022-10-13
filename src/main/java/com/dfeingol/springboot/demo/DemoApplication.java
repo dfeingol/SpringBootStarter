c659a7508486bb8d6635affe87423113632cdae3
David Feingold <softintegrationdf@gmail.com>
2022-10-13 10:46:18 -0400

    updated changed

--- src/main/java/com/dfeingol/springboot/demo/DemoApplication.java
+++ src/main/java/com/dfeingol/springboot/demo/DemoApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// cool feature
 @SpringBootApplication
 public class DemoApplication implements CommandLineRunner {
 
